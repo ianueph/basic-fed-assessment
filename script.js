eeca4e1e202064616cadbb18a7b0ec5103887afd
ianueph <daniel.angeloianmiguel@gmail.com>
2026-05-28 21:20:40 +0800

    mega feature: implement shopping cart

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const data = {
     "products": [
         {
+            "id": 1,
             "name": "BLÅHAJ (Large)",
             "price": 1080,
             "currency": "PHP",
@@ -8,6 +9,7 @@ const data = {
             "image": "BLÅHAJ (Large).jpg"
         },
         {
+            "id": 2,
             "name": "BLÅHAJ (Small)",
             "price": 350,
             "currency": "PHP",
@@ -15,6 +17,7 @@ const data = {
             "image": "BLÅHAJ (Small).jpg"
         },
         {
+            "id": 3,
             "name": "GREJSIMOJS",
             "price": 1205.50,
             "currency": "PHP",
@@ -22,6 +25,7 @@ const data = {
             "image": "GREJSIMOJS.jpg"
         },
         {
+            "id": 4,
             "name": "GOSIG GOLDEN",
             "price": 500,
             "currency": "PHP",
@@ -29,6 +33,7 @@ const data = {
             "image": "GOSIG GOLDEN.jpg"
         },
         {
+            "id": 5,
             "name": "DJUNGELSKOG",
             "price": 69,
             "currency": "PHP",
@@ -36,6 +41,7 @@ const data = {
             "image": "DJUNGELSKOG.jpg"
         },
         {
+            "id": 6,
             "name": "DVÄRGHARE",
             "price": 679,
             "currency": "PHP",
@@ -43,6 +49,7 @@ const data = {
             "image": "DVÄRGHARE.jpg"
         },
         {
+            "id": 7,
             "name": "SANDLÖPARE",
             "price": 240,
             "currency": "PHP",
@@ -50,6 +57,7 @@ const data = {
             "image": "SANDLÖPARE.jpg"
         },
         {
+            "id": 8,
             "name": "LILLEPLUTT",
             "price": 3200,
             "currency": "PHP",
@@ -57,6 +65,7 @@ const data = {
             "image": "LILLEPLUTT.jpg"
         },
         {
+            "id": 9,
             "name": "BLÅHAJ (Large)",
             "price": 1080,
             "currency": "PHP",
@@ -64,6 +73,7 @@ const data = {
             "image": "BLÅHAJ (Large).jpg"
         },
         {
+            "id": 10,
             "name": "BLÅHAJ (Small)",
             "price": 350,
             "currency": "PHP",
@@ -71,6 +81,7 @@ const data = {
             "image": "BLÅHAJ (Small).jpg"
         },
         {
+            "id": 11,
             "name": "GREJSIMOJS",
             "price": 1205.50,
             "currency": "PHP",
@@ -78,6 +89,7 @@ const data = {
             "image": "GREJSIMOJS.jpg"
         },
         {
+            "id": 12,
             "name": "GOSIG GOLDEN",
             "price": 500,
             "currency": "PHP",
@@ -85,6 +97,7 @@ const data = {
             "image": "GOSIG GOLDEN.jpg"
         },
         {
+            "id": 13,
             "name": "DJUNGELSKOG",
             "price": 69,
             "currency": "PHP",
@@ -92,6 +105,7 @@ const data = {
             "image": "DJUNGELSKOG.jpg"
         },
         {
+            "id": 14,
             "name": "DVÄRGHARE",
             "price": 679,
             "currency": "PHP",
@@ -99,6 +113,7 @@ const data = {
             "image": "DVÄRGHARE.jpg"
         },
         {
+            "id": 15,
             "name": "SANDLÖPARE",
             "price": 240,
             "currency": "PHP",
@@ -106,6 +121,7 @@ const data = {
             "image": "SANDLÖPARE.jpg"
         },
         {
+            "id": 16,
             "name": "LILLEPLUTT",
             "price": 3200,
             "currency": "PHP",
@@ -113,6 +129,7 @@ const data = {
             "image": "LILLEPLUTT.jpg"
         },
         {
+            "id": 17,
             "name": "BLÅHAJ (Large)",
             "price": 1080,
             "currency": "PHP",
@@ -120,6 +137,7 @@ const data = {
             "image": "BLÅHAJ (Large).jpg"
         },
         {
+            "id": 18,
             "name": "BLÅHAJ (Small)",
             "price": 350,
             "currency": "PHP",
@@ -127,6 +145,7 @@ const data = {
             "image": "BLÅHAJ (Small).jpg"
         },
         {
+            "id": 19,
             "name": "GREJSIMOJS",
             "price": 1205.50,
             "currency": "PHP",
@@ -134,6 +153,7 @@ const data = {
             "image": "GREJSIMOJS.jpg"
         },
         {
+            "id": 20,
             "name": "GOSIG GOLDEN",
             "price": 500,
             "currency": "PHP",
@@ -141,6 +161,7 @@ const data = {
             "image": "GOSIG GOLDEN.jpg"
         },
         {
+            "id": 21,
             "name": "DJUNGELSKOG",
             "price": 69,
             "currency": "PHP",
@@ -148,6 +169,7 @@ const data = {
             "image": "DJUNGELSKOG.jpg"
         },
         {
+            "id": 22,
             "name": "DVÄRGHARE",
             "price": 679,
             "currency": "PHP",
@@ -155,6 +177,7 @@ const data = {
             "image": "DVÄRGHARE.jpg"
         },
         {
+            "id": 23,
             "name": "SANDLÖPARE",
             "price": 240,
             "currency": "PHP",
@@ -162,6 +185,7 @@ const data = {
             "image": "SANDLÖPARE.jpg"
         },
         {
+            "id": 24,
             "name": "LILLEPLUTT",
             "price": 3200,
             "currency": "PHP",
@@ -169,6 +193,7 @@ const data = {
             "image": "LILLEPLUTT.jpg"
         },
         {
+            "id": 25,
             "name": "BLÅHAJ (Large)",
             "price": 1080,
             "currency": "PHP",
@@ -176,6 +201,7 @@ const data = {
             "image": "BLÅHAJ (Large).jpg"
         },
         {
+            "id": 26,
             "name": "BLÅHAJ (Small)",
             "price": 350,
             "currency": "PHP",
@@ -183,6 +209,7 @@ const data = {
             "image": "BLÅHAJ (Small).jpg"
         },
         {
+            "id": 27,
             "name": "GREJSIMOJS",
             "price": 1205.50,
             "currency": "PHP",
@@ -190,6 +217,7 @@ const data = {
             "image": "GREJSIMOJS.jpg"
         },
         {
+            "id": 28,
             "name": "GOSIG GOLDEN",
             "price": 500,
             "currency": "PHP",
@@ -197,6 +225,7 @@ const data = {
             "image": "GOSIG GOLDEN.jpg"
         },
         {
+            "id": 29,
             "name": "DJUNGELSKOG",
             "price": 69,
             "currency": "PHP",
@@ -204,6 +233,7 @@ const data = {
             "image": "DJUNGELSKOG.jpg"
         },
         {
+            "id": 30,
             "name": "DVÄRGHARE",
             "price": 679,
             "currency": "PHP",
@@ -211,6 +241,7 @@ const data = {
             "image": "DVÄRGHARE.jpg"
         },
         {
+            "id": 31,
             "name": "SANDLÖPARE",
             "price": 240,
             "currency": "PHP",
@@ -218,6 +249,7 @@ const data = {
             "image": "SANDLÖPARE.jpg"
         },
         {
+            "id": 32,
             "name": "LILLEPLUTT",
             "price": 3200,
             "currency": "PHP",
@@ -229,9 +261,13 @@ const data = {
 
 const items_per_page = 6;
 
-const getTotalPages = (items_per_page) => {
-    return Math.ceil(data.products.length / items_per_page);
+const getTotalPages = (length, items_per_page) => {
+    return Math.ceil(length / items_per_page);
 }
+
+const getProductById = (id) => {
+    return data.products.find(product => product.id === id);
+};
 
 const loadProducts = (currentPage) => {
     const productGrid = document.querySelector(".products-list__grid");
@@ -251,17 +287,28 @@ const loadProducts = (currentPage) => {
             <h2> ${product.name} </h2> 
             <h3> ${product.price} ${product.currency} </h3>
             <h3> remaining: ${product["qty-remaining"]} </h3>
-            <button> Add to cart! </button>
+            <input type="number" value="1" id="${product.id}-qty-add"> 
         `;  
 
+        const button = document.createElement("button");
+        button.innerHTML = "Add to cart!";
+        button.addEventListener('click', () => {
+            addToCart(
+                product.id,
+                document.getElementById(`${product.id}-qty-add`).value
+            );
+            loadCart(1);
+        }); 
+
+        card.appendChild(button);
         productGrid.appendChild(card);
     }
 
-    loadPagination(currentPage, getTotalPages(items_per_page));
+    loadPagination(currentPage, getTotalPages(data.products.length, items_per_page), loadProducts, ".products-list__pagination");
 }
 
-const loadPagination = (currentPage, totalPages) => {
-    const paginationComponent = document.querySelector(".products-list__pagination");
+const loadPagination = (currentPage, totalPages, onPageChange, paginationClass) => {
+    const paginationComponent = document.querySelector(paginationClass);
 
     paginationComponent.innerHTML = "";
 
@@ -308,19 +355,99 @@ const loadPagination = (currentPage, totalPages) => {
 
         const button = document.createElement("button");
         button.addEventListener('click', () => {
-            loadProducts(page);
+            onPageChange(page);
         })
-
-        button.classList.add("products-list__pagination-button");
-
-        if (page === currentPage) {
-            button.classList.add("active");
-        }
-
         button.textContent = page;
 
         paginationComponent.appendChild(button);
     });
 };
+
+let cart = new Map();
+const addToCart = (id, qty) => {
+    const parsedQty = parseInt(qty);
+    const current = parseInt(cart.get(id) ?? 0);
+
+    if ((current + parsedQty) > getProductById(id)["qty-remaining"]) { return; }
+
+    cart.set(id, current + parsedQty);
+
+    const product = getProductById(id);
+    alert(`You added ${parsedQty} of ${product.name} into your cart! \nTotal added: +${product.price*parsedQty} ${product.currency}
+        `);
+
+    console.log(cart.get(id));
+};
+
+const subtractFromCart = (id, qty) => {
+    const parsedQty = parseInt(qty);
+    const current = parseInt(cart.get(id) ?? 0);
+
+    if ( parsedQty < 0 ) {return;}
+    if ( parsedQty > current ) {return;}
+    if ( (current - parsedQty) < 0) {return;}
+    
+    if ( (current - parsedQty) === 0 ) {
+        cart.delete(id);
+    } else {
+        cart.set(id, current - parsedQty);
+    }
+
+    const product = getProductById(id);
+    alert(`You removed ${parsedQty} of ${product.name} from your cart!`);
+
+    console.log(cart.get(id));
+}
+
+const loadCart = (currentPage) => {
+    const items_per_page = 4;
+    const shoppingCartGrid = document.querySelector(".shopping-cart__grid");
+    shoppingCartGrid.innerHTML = "";
+
+    const items = Array.from(cart.entries()); 
+    const skip = (currentPage - 1) * items_per_page;
+    
+    for (let i = skip; i <= skip + 3; i++) {
+        if (i >= items.length) { break; }
+
+        const [id, qty] = items[i]
+        const product = getProductById(id);
+
+        const card = document.createElement("div");
+
+        card.classList.add("border");
+        card.classList.add("shopping-cart__item")
+        card.innerHTML = `
+            <img src="./resources/images/products/${[product.name]}.jpg" alt="${[product.name]}">
+            <h2> ${product.name} </h2> 
+            <h3> ${product.price} ${product.currency} </h3>
+            <h3>qty in cart: ${qty}</h3>
+            <input type="number" id="${product.id}-qty-sub" value="1">
+        `;  
+
+        const button = document.createElement("button");
+        button.innerHTML = "Remove from cart.";
+        button.addEventListener('click', () => {
+            subtractFromCart(
+                product.id,
+                document.getElementById(`${product.id}-qty-sub`).value
+            );
+            loadCart(1);
+        }); 
+
+        card.appendChild(button);
+        shoppingCartGrid.appendChild(card);
+    }
+
+    loadPagination(currentPage, getTotalPages(items.length, 4), loadCart, ".shopping-cart__pagination")
+}
+
+const modal = document.querySelector(".modal-wrapper");
+const btn = document.querySelector(".page-heading__shopping-cart--toggleModal");
+
+btn.addEventListener('click', () => {
+    loadCart(1);
+    modal.classList.toggle('active');
+})
 
 loadProducts(1);
